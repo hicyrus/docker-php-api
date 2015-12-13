736b43cdb4ea869d52ff6cbc21bbcf2e7160fb51
hicyrus <zhu2111@126.com>
2015-12-13 17:11:40 +0800

    composer init

--- src/Docker/Http/Client/HttpClient.php
+++ src/Docker/Http/Client/HttpClient.php
@@ -6,6 +6,7 @@ namespace Docker\Http\Client;
 
 class  HttpClient {
 
+	//post request
 	public static  function post($url,$data=array())
 	{
 		$curl = curl_init();
@@ -23,6 +24,28 @@ class  HttpClient {
 		$output = curl_exec($curl);
 		echo $output;
 		curl_close($curl);
+	}
+
+
+	//get request
+	public static  function get($url)
+	{
+		//初始化
+	　　$ch = curl_init();
+
+	　　//设置选项，包括URL
+	　　curl_setopt($ch, CURLOPT_URL, $url);
+	　　curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	　　curl_setopt($ch, CURLOPT_HEADER, 0);
+
+	　　//执行并获取HTML文档内容
+	　　$output = curl_exec($ch);
+
+	　　//释放curl句柄
+	　　curl_close($ch);
+	　　//打印获得的数据
+	　　print_r($output);
+
 	}
 
 
